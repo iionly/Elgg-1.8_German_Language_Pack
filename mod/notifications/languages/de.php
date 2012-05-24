81c932a643c2bedde711b91d4708b0e6b1f4542c
iionly <iionly@gmx.de>
2012-05-24 16:15:19 +0200

    Initial commit

---
+++ mod/notifications/languages/de.php
@@ -0,0 +1,26 @@
+<?php
+
+$german = array(
+
+	'friends:all' => 'Alle Freunde',
+
+	'notifications:subscriptions:personal:description' => 'Benachrichtigt werden, wenn es zu Deinen Beiträgen auf der Seite Rückmeldungen gibt.',
+	'notifications:subscriptions:personal:title' => 'Persönliche Benachrichtigungseinstellungen',
+
+	'notifications:subscriptions:friends:title' => 'Freundeslisten',
+	'notifications:subscriptions:friends:description' => 'Um gleiche Benachrichtigungseinstellungen für alle Freunde, die in einer Deiner Freundeslisten enthalten sind, festzulegen, kannst Du Deine verfügbaren Freundeslisten hier an- und abwählen. Dies wird aber auch die freundesspezifischen Benachrichtigungseinstellungen, die weiter unten verfügbar sind, beeinflussen. Die erste Liste wurde automatisch erstellt und enthält alle Deine Freunde.',
+	'notifications:subscriptions:collections:edit' => 'Um Deine Freundeslisten zu bearbeiten, hier klicken.',
+
+	'notifications:subscriptions:changesettings' => 'Benachrichtigungen',
+	'notifications:subscriptions:changesettings:groups' => 'Gruppen-Benachrichtigungen',
+
+	'notifications:subscriptions:title' => 'Freundesspezifische Benachrichtigungseinstellungen',
+	'notifications:subscriptions:description' => 'Um benachrichtigt zu werden, wenn Deine Freunde neue Beiträge erstellen, kannst Du für jeden einzelnen Deiner Freunde im Folgenden spezifische Benachrichtungseinstellungen festlegen.',
+
+	'notifications:subscriptions:groups:description' => 'Um benachrichtigt zu werden, wenn neue Beiträge zu einer Gruppe hinzugefügt werden, in der Du Mitglied bist, kannst Du im Folgenden gruppenspezifische Benachrichtigungseinstellungen festlegen.',
+
+	'notifications:subscriptions:success' => 'Deine Benachrichtigungseinstellungen wurden gespeichert.',
+
+);
+
+	add_translation("de", $german);
---
+++ mod/tinymce/languages/de.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * TinyMCE language pack.
+ *
+ * @package ElggTinyMCE
+ */
+
+$german = array(
+	'tinymce:remove' => "Editor deaktivieren",
+	'tinymce:add' => "Editor aktivieren",
+	'tinymce:word_count' => 'Anzahl Worte: ',
+);
+
+add_translation("de", $german);
